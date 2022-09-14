eb448fd73f10f21ab2278d10adb9c6f3aa66f557
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-09-14 14:35:22 +0300

    Fixed MappingProfile

--- src/ProfilerService.Grpc/MappingProfile.cs
+++ src/ProfilerService.Grpc/MappingProfile.cs
@@ -8,21 +8,25 @@ public class MappingProfile : AutoMapper.Profile
     {
         CreateMap<Service.Grpc.CreateProfileRequest, Profile>()
             .ForMember(dest => dest.DiscrodId, opt => opt.MapFrom(src => src.DiscordId));
+
+        CreateMap<string, Guid>()
+            .ConvertUsing((x, res) => res = Guid.TryParse(x, out var id) ? id : Guid.Empty);
+        CreateMap<Guid?, string>()
+            .ConvertUsing((x, res) => res = x?.ToString() ?? string.Empty);
+
+        CreateMap<string, DateTime>()
+            .ConvertUsing((x, res) => res = DateTime.TryParse(x, out var dateTime) ? dateTime : DateTime.MinValue);
+        CreateMap<DateTime, string>()
+            .ConvertUsing((x, res) => res = x.ToString());
+
         CreateMap<Profile, Service.Grpc.Profile>()
             .ForMember(dest => dest.DiscordId, opt => opt.MapFrom(src => src.DiscrodId))
             .ForMember(dest => dest.Points, opt => opt.MapFrom(src => src.Points))
-            .ForMember(dest => dest.WaxWallet, opt => opt.MapFrom(src => src.WaxWallet ?? string.Empty))
-            .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreationDate.ToString()))
-            .ForMember(dest => dest.LoseCount, opt => opt.MapFrom(src => src.LoseCount))
-            .ForMember(dest => dest.WinCount, opt => opt.MapFrom(src => src.WinCount))
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
-        CreateMap<Service.Grpc.Profile, Profile>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.Parse(src.Id)))
-            .ForMember(dest => dest.DiscrodId, opt => opt.MapFrom(src => src.DiscordId))
             .ForMember(dest => dest.WaxWallet, opt => opt.MapFrom(src => src.WaxWallet))
-            .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => DateTime.Parse(src.CreationDate)))
+            .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreationDate))
             .ForMember(dest => dest.LoseCount, opt => opt.MapFrom(src => src.LoseCount))
             .ForMember(dest => dest.WinCount, opt => opt.MapFrom(src => src.WinCount))
-            .ForMember(dest => dest.Points, opt => opt.MapFrom(src => src.Points));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ReverseMap();
     }
 }
